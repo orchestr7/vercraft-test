a49d7d4e67835e73eb9f049fc8fd46bd03bce714
akuleshov7 <andrewkuleshov7@gmail.com>
2025-01-22 20:37:23 +0300

    commit 12

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("jvm") version "2.0.0"
-    id("com.akuleshov7.vercraft.plugin-gradle") version("0.3.0")
+    id("com.akuleshov7.vercraft.plugin-gradle") version("0.5.0")
 }
 
 group = "org.example"
